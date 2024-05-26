9b5fe16227706c2555377ed4e1e9eaa1298b6fa2
luuizz <luu.izz@hotmail.com>
2024-05-26 13:51:04 -0300

    feat: adicionado preload no app

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,35 @@
+"use client"
 import HomeSectionArtDigital from "@/components/Home/ArtDigitalSection";
 import HomeFrontSection from "@/components/Home/FrontSection";
 import HomeHeroSection from "@/components/Home/HeroSection";
 import HomeNewsletterSection from "@/components/Home/NewsletterSection";
+import Preload from "@/components/Preload";
+import { AnimatePresence } from "framer-motion";
+import { useEffect } from "react";
+import { usePreload } from "./context/PreloadContext";
 
 export default function Home() {
+
+  const { isLoading, setIsLoading } = usePreload();
+
+  useEffect(() => {
+    (
+      async () => {
+        setTimeout(() => {
+          setIsLoading(false);
+          document.body.style.cursor = 'default'
+          document.body.style.overflow = 'auto'
+          window.scrollTo(0,0);
+        }, 2000)
+      }
+    )()
+  }, [])
+
   return (
     <>
+      <AnimatePresence mode="wait">
+        {isLoading && <Preload />}
+      </AnimatePresence>
       <HomeHeroSection />
       <HomeSectionArtDigital />
       <HomeFrontSection />
